3aadaca5632b9847e7ffaa431a08666251bfcff0
gandos <rino.gdos@gmail.com>
2018-02-23 19:52:32 +0800

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -26,11 +26,18 @@ if not args.url:
 	sys.exit(1)
 
 def send_request(url, fname):
+	if not url:
+		print("Empty url, aborting")
+		return
+	
 	if not url.startswith("http"):
 		url = "http://" + url
 		
 	url_o = urlsplit(url)
-	print(url_o)
+	
+	if not url_o.netloc:
+		print("Malforing url, aborting")
+		return
 	
 	if url_o.scheme == 'http':
 		conn = client.HTTPConnection(url_o.netloc)
@@ -45,29 +52,39 @@ def send_request(url, fname):
 		
 	conn.request("GET", path)
 	rsp = conn.getresponse()	
-	doc = html.fromstring(rsp.read())
+	doc = html.fromstring(rsp.read().decode("utf-8"))
 	
 	print (url, ': HTTP ', rsp.status, '-->', fname)
 	#print(etree.tostring(doc, encoding='unicode', pretty_print=True))
-	with open(fname, "w") as file:
+	with open(fname, "w", encoding="utf-8") as file:
 		file.write(etree.tostring(doc, encoding = 'unicode', pretty_print=True))
 
 	res = []
 	for child in doc.iter('a'):
 		href = child.get("href")
 		
-		if href and not href == "#" and (not args.pattern or args.pattern in href):
+		if href and not href == "#" and (not args.pattern or args.pattern in href):						
+			hname = "unknown_"+strftime("%Y%m%d-%H%M%S", gmtime())
+			
 			h_arr = href.split( "/" )
+			if len(h_arr) > 0:
+				hname = h_arr[len(h_arr)-1]
+				
+				if not hname and len(h_arr) > 1:
+					hname = h_arr[len(h_arr)-2]
+			
+			if "?" in hname:		
+				hname = hname[0:hname.find("?")]
+				
 		
 			if not href.startswith("http") and href.startswith("/"):
 				href = url_o.scheme +"://" +url_o.netloc +href
 			
-			res.append(tuple((h_arr[len(h_arr)-1], href)))
+			res.append(tuple((hname, href)))
 	
 	return res
 
-def traverse(tuples, level):
-	
+def traverse(tuples, level):	
 	if level <= args.level:
 		for t in tuples:
 			try:
@@ -76,10 +93,20 @@ def traverse(tuples, level):
 				sleep(args.delay)
 			
 				traverse(r, level+1)
-			except:
+			except Exception as e:
 				print("error processing ", t[1])		
+				print(e)
 		
-
+def request_test(url):
+	url_o = urlsplit(url)
+	conn = client.HTTPSConnection(url_o.netloc)
+			
+	conn.request("GET", url_o.path)
+	rsp = conn.getresponse()	
+	#print(rsp.read().decode('utf-8'))
+	with open("./result.html", "w", encoding="utf-8") as file:
+		file.write(rsp.read().decode("utf-8"))
+	
 def main():
 	url = args.url
 	
@@ -90,6 +117,7 @@ def main():
 	
 	t = tuple(('root',url))
 	traverse([t], 1)
-
+	#request_test("https://www.alphapolis.co.jp/novel/901123427/931067230")
+	
 if __name__ == "__main__":
 	main()
